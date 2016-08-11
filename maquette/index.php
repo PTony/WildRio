3696a04a90729e08cc0015fda94551b664a393c3
PTony <jokr.01@gmail.com>
2016-08-11 10:50:37 +0200

    creation projet symfony

---
+++ maquette/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Accueil</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <header class="header col-md-12"></header>
+    <div class="container">
+
+      <div class="epreuves row"> 
+        <h2>Prochaines épreuves</h2>
+
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img src="../Resources/natation.jpg" alt="...">
+            <div class="caption">
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p class="text-right"><a href="#" class="btn btn-primary" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img src="../Resources/article_escrime.jpg" alt="...">
+            <div class="caption">
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p class="text-right"><a href="#" class="btn btn-primary" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img src="../Resources/fUJqF-ZX.jpg" alt="...">
+            <div class="caption">
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p class="text-right"><a href="#" class="btn btn-primary" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="media row">
+        <div class="col-md-offset-1 col-md-6">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+
+        <div class="col-md-4 text-panel">
+          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        </div>
+
+      </div>
+      
+      <div class="footer row">
+        <footer class="col-md-offset-4 col-md-4">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="../Resources/facebook.png">
+            </div>
+            <div class="col-md-4">
+              <img src="../Resources/twitter.png">
+            </div>
+            <div class="col-md-4">
+              <img src="../Resources/googleplus.png">
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
